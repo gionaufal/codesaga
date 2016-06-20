4f258442cdf81ab9a6a2c015e29a2ae09276dbca
gionaufal <g10v45+github@gmail.com>
2016-06-20 08:02:01 -0300

    update agenda exercise

--- agenda/agenda.rb
+++ agenda/agenda.rb
@@ -3,22 +3,41 @@ def strip_text(text)
 end
 
 def tel_description(tipo)
-  if tipo == 'C'
-    "Comercial"
-  elsif tipo == "R"
-    "Residencial"
-  elsif tipo == 'M'
-    "Celular"
+  # if tipo == 'C'
+  #   "Comercial"
+  # elsif tipo == "R"
+  #   "Residencial"
+  # elsif tipo == 'M'
+  #   "Celular"
+  # end
+  case tipo
+  when 'C'
+    'Comercial'
+  when 'R'
+    'Residencial'
+  when 'M'
+    'Celular'
+  else
+    'Não identificado'
   end
-
 end
 
-puts 'Entre com seu nome: '
-nome = gets.strip
-puts "Seu nome é: #{nome}"
-puts "Entre seu número de telefone: "
-telefone = gets
-puts "Entre o tipo de telefone: "
-puts "Comercial [C], Residencial [R] ou Celular [M]"
-tipo_telefone = gets
-puts "#{strip_text(nome)} - #{strip_text(telefone)} - #{tel_description(strip_text(tipo_telefone))}"
+def get_register
+  puts 'Entre com seu nome: '
+  nome = gets.strip
+  puts "Seu nome é: #{nome}"
+  puts "Entre seu número de telefone: "
+  telefone = gets
+  puts "Entre o tipo de telefone: "
+  puts "Comercial [C], Residencial [R] ou Celular [M]"
+  tipo_telefone = gets
+  puts "#{strip_text(nome)} - #{strip_text(telefone)} - #{tel_description(strip_text(tipo_telefone))}"
+end
+
+continua = '1'
+while continua == '1'
+  get_register
+  puts 'Deseja incluir um novo registro?'
+  puts '[1] Sim [0] Não'
+  continua = strip_text(gets)
+end
